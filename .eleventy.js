797648c6b62a77f8e791a350d7b957ae7ed9a1cf
drusin <dawidrusin90@gmail.com>
2024-03-26 22:59:41 +0100

    styling, syntax highlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
   // necessary to automatically prepend the prefix to internal links
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy("src");
   eleventyConfig.addPassthroughCopy({ "node_modules/@picocss/pico/css/pico.min.css": "pico.min.css" });
+  eleventyConfig.addPassthroughCopy({ "node_modules/prismjs/themes/prism-okaidia.min.css": "prism-okaidia.min.css" });
   return {
     dir: {
       input: "content"
